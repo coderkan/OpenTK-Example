abc1a347d41503b445b2a62e478967b1085cadf9
coderkan <erkanguzeler@gmail.com>
2016-11-17 16:33:40 +0300

    first commit

---
+++ OpenTkExample/Game.cs
@@ -0,0 +1,49 @@
+﻿using OpenTK;
+using System;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+
+namespace OpenTkExample
+{
+	class Game :GameWindow
+	{
+		public Game():
+			base(512,512,new OpenTK.Graphics.GraphicsMode(32, 24, 0, 4))
+		{
+
+		}
+		
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			GL.ClearColor(Color.Blue);
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			base.OnRenderFrame(e);
+
+			GL.Viewport(0, 0, Width, Height);
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+			GL.Enable(EnableCap.DepthTest);
+
+
+			GL.Flush();
+			SwapBuffers();
+		}
+
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			base.OnUpdateFrame(e);
+		}
+
+
+
+
+
+
+	}
+}
---
+++ OpenTkExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenTkExample
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			using (Game game = new Game())
+			{
+
+				game.Run(30, 30);
+
+			}
+
+			//Application.EnableVisualStyles();
+			//Application.SetCompatibleTextRenderingDefault(false);
+			//Application.Run(new Form1());
+		}
+	}
+}
